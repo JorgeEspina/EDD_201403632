a68845e390fddde534a27d94801bab01936bc084
JorgeEspina <canchemolinas@gmail.com>
2017-12-27 23:45:33 -0600

    primera parte, falta pasar la matriz y espejo a este proyecto

---
+++ Naval Wars/Naval Wars/Estructuras/Espejo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Naval_Wars.Estructuras
+{
+    public class Espejo
+    {
+    }
+}
---
+++ Naval Wars/Naval Wars/Estructuras/Matriz.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Naval_Wars.Estructuras
+{
+    public class Matriz
+    {
+
+    }
+}
